b29fb1333090823ad76bb579dfd14504e63d0b33
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-10-16 16:43:51 +0300

    Tusk3

---
+++ IDriveable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class IDriveable
+    {
+        public ТипТранспорту Тип { get; set; }
+        public double Швидкість { get; set; }
+        public double Розмір { get; set; }
+
+        public ТранспортнийЗасіб(ТипТранспорту тип, double швидкість, double розмір)
+        {
+            Тип = тип;
+            Швидкість = швидкість;
+            Розмір = розмір;
+        }
+
+        public void Рухатися()
+        {
+            Console.WriteLine($"Транспортний засіб типу {Тип} рухається зі швидкістю {Швидкість} км/год.");
+        }
+
+        public void Зупинитися()
+        {
+            Console.WriteLine($"Транспортний засіб типу {Тип} зупинився.");
+        }
+        interface IDriveable
+        {
+            void Рухатися();
+            void Зупинитися();
+        }
+
+        // Клас, що імітує рух транспортних засобів
+        class СимуляторРуху
+        {
+            public static void Main(string[] args)
+            {
+                Дорога дорога = new Дорога(10.0, 3.5, 2);
+                ТранспортнийЗасіб авто = new ТранспортнийЗасіб(ТипТранспорту.Автомобіль, 60.0, 4.5);
+                ТранспортнийЗасіб вантажівка = new ТранспортнийЗасіб(ТипТранспорту.Вантажівка, 40.0, 6.0);
+
+                авто.Рухатися();
+                вантажівка.Рухатися();
+
+                авто.Зупинитися();
+                вантажівка.Зупинитися();
+            }
+}
---
+++ Road.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Road
+    {
+        public double Довжина { get; set; }
+        public double Ширина { get; set; }
+        public int КількістьСмуг { get; set; }
+        public int РівеньТрафіку { get; set; }
+
+        public Дорога(double довжина, double ширина, int кількістьСмуг)
+        {
+            Довжина = довжина;
+            Ширина = ширина;
+            КількістьСмуг = кількістьСмуг;
+            РівеньТрафіку = 0;
+        }
+        // Перелік можливих типів транспортних засобів
+        enum ТипТранспорту
+        {
+            Автомобіль,
+            Вантажівка,
+            Автобус
+        }
+    }
+}
